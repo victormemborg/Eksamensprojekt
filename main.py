4c26cc5ad9b7cfd8ffb80c761f8342e97880f7ca
youngchadm <memborg57@gmail.com>
2022-04-20 13:17:54 +0200

    Integration med cancer modellen
    *Virker endnu ikke

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 #Importerer libraries 
 import numpy as np
 import os
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect, url_for
 import pickle
 from werkzeug.utils import secure_filename
+import tensorflow.keras
+from PIL import Image, ImageOps
 
 app = Flask(__name__)
 
@@ -52,7 +54,43 @@ def upload_file():
    if request.method == 'POST':
       f = request.files['filename[]']
       f.save(os.path.join(app.config['UPLOAD_FOLDER'], "image.png"))
-      return 'file uploaded successfully'
+      return redirect(url_for('resultat'))
+
+@app.route("/result")
+def resultat():
+    np.set_printoptions(suppress=True)
+
+    #Loader vores teachable machine.
+    model = tensorflow.keras.models.load_model('static\data\Leukemia\keras_model.h5')
+
+    #Former vores teachable machine data ind i et array.
+    data = np.ndarray(shape=(1, 224, 224, 4), dtype=np.float32)
+    image = Image.open('uploads\image.png')
+
+    #Resizer billedet som brugeren har taget, så det passer perfekt til vores data.
+    size = (224, 224)
+    image = ImageOps.fit(image, size, Image.ANTIALIAS)
+
+    #Laver billedet som brugeren har taget om til et array, så den kan sammenligne med den data som AI har.
+    image_array = np.asarray(image)
+
+    # Normalize the image
+    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+
+    #Loader billedet ind i et array.
+    data[0] = normalized_image_array
+
+
+    #Sammenligner hvor meget billedet, som brugeren har taget, matcher med det data som AI har. 1 betyder at billedet er identitsk med de data som AI har. 
+    prediction = model.predict(data)
+
+    #Afrunder til 2 decimaler, og ganger med 100, for at få tallet i en procent.
+    print("Resultater (hvor meget ligner billedet de to materialer):")
+    print("Start: ", np.round(prediction[0][0]*100, decimals=2), "%")
+    print("I udbrud: ", np.round(prediction[0][1]*100, decimals=2), "%")
+    print("Før behandling: ", np.round(prediction[0][2]*100, decimals=2), "%")
+    print("Efter behandling: ", np.round(prediction[0][3]*100, decimals=2), "%")
+    return render_template("resultat.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
