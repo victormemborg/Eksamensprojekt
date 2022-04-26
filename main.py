e7a9f4c8b94434e4c4bce1e959faf3ae010bc453
youngchadm <memborg57@gmail.com>
2022-04-26 09:38:55 +0200

    keras model virker

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 from flask import Flask, request, render_template, redirect, url_for
 import pickle
 from werkzeug.utils import secure_filename
-import tensorflow.keras
+from keras.models import load_model
 from PIL import Image, ImageOps
 
 app = Flask(__name__)
@@ -16,6 +16,43 @@ app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 
 #Loader pickle som der blev dannet i demo.ibynp. Denne bliver tildelt variablen "model"
 model = pickle.load(open("static\data\Risiko.pkl", "rb"))
+
+#ML funktion
+def ml():
+    #Loader modellen.
+    model = load_model('static\data\Leukemia\keras_model.h5')
+
+    #Opstiller array'et som der skal feedes til keras modellen.
+    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+    
+    #Loader billedet, som brugeren har uploadet
+    image = Image.open('uploads\image.png')
+    
+    #Resizer billedet til 224x224 og beskære det til midten.
+    size = (224, 224)
+    image = ImageOps.fit(image, size, Image.ANTIALIAS)
+
+    #Indsætter billedet i et numpy array.
+    image_array = np.asarray(image)
+
+    #Normaliserer array.
+    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+    data[0] = normalized_image_array
+
+    #Feeder modellen med dataen.
+    prediction = model.predict(data)
+
+    #variable der skal bruges på frontend siden, samt viser det procentvis.
+    global startfase
+    global udbrud 
+    global prebehandling
+    global probehandling
+    startfase = np.round(prediction[0][0]*100, decimals=2)
+    udbrud = np.round(prediction[0][1]*100, decimals=2)
+    prebehandling = np.round(prediction[0][2]*100, decimals=2)
+    probehandling = np.round(prediction[0][3]*100, decimals=2)
+
+
 
 #Laver et index route, som indlæser "index.html"
 @app.route("/")
@@ -58,39 +95,8 @@ def upload_file():
 
 @app.route("/result")
 def resultat():
-    np.set_printoptions(suppress=True)
-
-    #Loader vores teachable machine.
-    model = tensorflow.keras.models.load_model('static\data\Leukemia\keras_model.h5')
-
-    #Former vores teachable machine data ind i et array.
-    data = np.ndarray(shape=(1, 224, 224, 4), dtype=np.float32)
-    image = Image.open('uploads\image.png')
-
-    #Resizer billedet som brugeren har taget, så det passer perfekt til vores data.
-    size = (224, 224)
-    image = ImageOps.fit(image, size, Image.ANTIALIAS)
-
-    #Laver billedet som brugeren har taget om til et array, så den kan sammenligne med den data som AI har.
-    image_array = np.asarray(image)
-
-    # Normalize the image
-    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
-
-    #Loader billedet ind i et array.
-    data[0] = normalized_image_array
-
-
-    #Sammenligner hvor meget billedet, som brugeren har taget, matcher med det data som AI har. 1 betyder at billedet er identitsk med de data som AI har. 
-    prediction = model.predict(data)
-
-    #Afrunder til 2 decimaler, og ganger med 100, for at få tallet i en procent.
-    print("Resultater (hvor meget ligner billedet de to materialer):")
-    print("Start: ", np.round(prediction[0][0]*100, decimals=2), "%")
-    print("I udbrud: ", np.round(prediction[0][1]*100, decimals=2), "%")
-    print("Før behandling: ", np.round(prediction[0][2]*100, decimals=2), "%")
-    print("Efter behandling: ", np.round(prediction[0][3]*100, decimals=2), "%")
-    return render_template("resultat.html")
+    ml()
+    return render_template('resultat.html', start=startfase, udbrud=udbrud, prebe=prebehandling, probe=probehandling)
 
 if __name__ == "__main__":
     app.run(debug=True)
